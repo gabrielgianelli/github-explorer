32fa832819cf80c7d3f4e2be4f90ad860fa9ceb8
Gabriel Gianelli <gabrielgianelli@gmail.com>
2020-08-12 01:32:17 -0300

    feat: stylize details

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues } from './styles';
+import logoImg from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +11,55 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository:{params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars1.githubusercontent.com/u/24864079?s=400&u=d8f7a86d711b0716e2432339139aa5af988aae1c&v=4"
+            alt="Gabriel Gianelli"
+          />
+          <div>
+            <strong>go-barber</strong>
+            <p>descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1608</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="sadsd">
+          <div>
+            <strong>asdaasa</strong>
+            <p>assasa</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
